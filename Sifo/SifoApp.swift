6d830d8a39b5fc0444d0a2f1d4cc2066f2d2161f
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-09 21:09:15 +0300

    Initial Commit

---
+++ Sifo/SifoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SifoApp.swift
+//  Sifo
+//
+//  Created by Abdelrahman Mohamed on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SifoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SifoTests/SifoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SifoTests.swift
+//  SifoTests
+//
+//  Created by Abdelrahman Mohamed on 09.01.2026.
+//
+
+import Testing
+@testable import Sifo
+
+struct SifoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
